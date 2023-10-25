c1fa2d484d0bce59ac9a80df57801aac9c0ae418
krishnaveni21 <krishnaveni.kommana@gmail.com>
2023-10-24 19:14:32 -0500

    removed

--- src/main/java/com/test/springtest/SpringTestApplication.java
+++ src/main/java/com/test/springtest/SpringTestApplication.java
@@ -2,10 +2,11 @@ package com.test.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class SpringTestApplication {
 
